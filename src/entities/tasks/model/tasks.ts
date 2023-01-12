29aa71d168d5d359c4257876fe9c374aed522d3d
mlgmontage <kettleofrassel1@gmail.com>
2023-01-12 23:56:43 +0600

    feat: with store provider

---
+++ src/entities/tasks/model/tasks.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { Tasks } from "..";
+
+// TODO: Hard-coded data should be deleted later
+const initialState: Tasks = [
+  {
+    text: 'Watch "Green elephant"',
+    date: Date.now().toLocaleString(),
+    tags: ["productivity", "health"],
+    type: "basic",
+  },
+  {
+    text: 'Read book "Coralina',
+    date: Date.now().toLocaleString(),
+    tags: ["productivity", "education"],
+    type: "basic",
+  },
+  {
+    text: "Wash dishes",
+    date: Date.now().toLocaleString(),
+    tags: ["productivity"],
+    type: "basic",
+  },
+];
+
+const tasksSlice = createSlice({
+  name: "tasks",
+  initialState,
+  reducers: {},
+});
+
+export const taskReducer = tasksSlice.reducer;
